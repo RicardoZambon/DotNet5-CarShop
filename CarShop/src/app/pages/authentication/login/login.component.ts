3afff0da18867fb4091d97514b0e84dad8917e86
Ricardo Zambon <ricardo.zambon@smartm.com>
2021-05-12 16:08:48 -0300

    Add validation to login

--- CarShop/src/app/pages/authentication/login/login.component.ts
+++ CarShop/src/app/pages/authentication/login/login.component.ts
@@ -1,20 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-
-import { TranslateService } from './../../../shared/services/translate.service';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { from, of } from 'rxjs';
+import { concatMap, delay } from 'rxjs/operators';
+import Popover from 'bootstrap/js/dist/popover';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
-  constructor(public translateService: TranslateService) { }
+    @ViewChildren('popover') popovers!: QueryList<ElementRef>;
 
-  ngOnInit(): void {
-  }
+    loginForm!: FormGroup;
+    loading: boolean = false;
 
-  changeLanguage(code: string): void {
-      this.translateService.setLanguage(code);
-  }
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            rememberMe: [false]
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.popovers.map((x: ElementRef) => {
+            return new Popover(x.nativeElement, { content: '' })
+        });
+    }
+
+
+    hasFormErrors():boolean {
+        return this.loginForm.hasError('invalid');
+    }
+
+    submit() {
+        if (this.loginForm.valid) {
+            this.loginForm.disable();
+
+            const myArray = [1];
+            from(myArray).pipe(
+                concatMap(item => of(item).pipe(delay(5000)))
+            ).subscribe (timedItem => {
+                this.loginForm.enable();
+                this.loginForm.setErrors({'invalid': true});
+            });
+
+        } else {
+            Object.keys(this.loginForm.controls).forEach(field => {
+                const control = this.loginForm.get(field);
+                control?.markAsTouched({ onlySelf: true });
+            });
+        }
+    }
 }
