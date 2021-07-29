109537fddeb79be201d6a7bd294d5728b74f8e56
Ricardo Zambon <ricardo.zambon@smartm.com>
2021-07-29 15:49:52 -0300

    Fix bug with accordion collapse and show

--- CarShop/src/app/shared/components/edit/edit-item-title/edit-item-title.component.ts
+++ CarShop/src/app/shared/components/edit/edit-item-title/edit-item-title.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Collapse } from 'bootstrap';
 
 @Component({
     selector: 'app-edit-item-title',
@@ -6,15 +7,28 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['./edit-item-title.component.scss'],
     host: { class: 'accordion-item' }
 })
-export class EditItemTitleComponent implements OnInit {
+export class EditItemTitleComponent implements OnInit, AfterViewInit {
 
     @Input('id') id!: string;
-    
     @Input() label!: string;
+
+    @ViewChild('panel') panel!: ElementRef<HTMLDivElement>;
+    collapse!: Collapse;
+    collapsed: boolean = false;
 
 
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        this.collapse = new Collapse(this.panel.nativeElement);
+        this.collapse.show();
+    }
+
+    click(): void {
+        this.collapse.toggle();
+        this.collapsed = !this.collapsed;
     }
 }
