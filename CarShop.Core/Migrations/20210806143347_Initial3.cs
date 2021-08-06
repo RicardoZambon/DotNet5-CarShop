f6d3bf09e19fd5786bf813978579c85b3e6f6877
Ricardo Zambon <ricardo.zambon@smartm.com>
2021-08-06 11:52:02 -0300

    Updating env variables

---
+++ CarShop.Core/Migrations/20210806143347_Initial3.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarShop.Core.Migrations
+{
+    public partial class Initial3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Roles",
+                type: "bit",
+                nullable: false,
+                defaultValueSql: "0",
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "hD4kR9TOvVMU6SaGOQP/r1wWVaThyVyZC8FVl6KMN3Vn6Sd82wAdxtbXod0gXRy2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Roles",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValueSql: "0");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "q48RJrWjmws22xTCgOX2Z618H5+wtNTGivsdHuZra1uLEeiH6HXLmL3VyYLkxkf9");
+        }
+    }
+}
